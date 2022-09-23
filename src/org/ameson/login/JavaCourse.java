447cf5b1e4d79722e67ce65ac68dd1ef4a29ab37
Nandhini1223 <nandhinivaradhan1997@gmail.com>
2022-09-23 13:59:55 +0530

    sample done

---
+++ src/org/ameson/login/JavaCourse.java
@@ -0,0 +1,24 @@
+package org.ameson.login;
+
+public class JavaCourse { 
+	
+	private void courseDetails() {
+		// TODO Auto-generated method stub
+
+	} 
+	{// TODO Auto-generated method stub
+     System.out.println("gomathi venky");
+}
+	
+	public static void main(String[] args) {
+		
+		
+		JavaCourse od= new JavaCourse();
+		
+		od.courseDetails();
+		
+		
+		
+		
+	}
+}
